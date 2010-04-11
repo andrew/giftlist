8dbc5eac5e1d0fa19a3991530089b6ccaea5463b
Andrew Nesbitt <andrewnez@gmail.com>
2010-04-11 16:54:15 +0100

    Flash notice styles

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -5,6 +5,7 @@ class ReservationsController < ApplicationController
 
     response_for :create do |format|
       format.html do
+        flash[:notice] = "Thankyou for reserving the #{@reservation.gift}"
         Notifier.deliver_new_reservation(@reservation)
         redirect_to gifts_path
       end
