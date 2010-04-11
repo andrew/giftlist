a7cafa28376fab51dd763a6caa1b235cc2c4aa2c
Andrew Nesbitt <andrewnez@gmail.com>
2010-04-11 16:07:47 +0100

    direct back to the gifts after making a reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,5 +2,9 @@ class ReservationsController < ApplicationController
   make_resourceful do
     actions :all
     belongs_to :gift
+
+    response_for :create do |format|
+      format.html { redirect_to gifts_path }
+    end
   end
 end
