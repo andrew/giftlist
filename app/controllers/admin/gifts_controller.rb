213a6a9f96ed97a6591a3f80abe44223e95145b1
Andrew Nesbitt <andrewnez@gmail.com>
2010-04-12 23:23:29 +0100

    Removed admin section

--- app/controllers/admin/gifts_controller.rb
+++
@@ -1,5 +0,0 @@
-class Admin::GiftsController < Admin::ApplicationController
-  make_resourceful do
-    actions :all
-  end
-end
--- app/controllers/admin/reservations_controller.rb
+++
@@ -1,6 +0,0 @@
-class Admin::ReservationsController < Admin::ApplicationController
-  make_resourceful do
-    actions :all
-    belongs_to :gift
-  end
-end
