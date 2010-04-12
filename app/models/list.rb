131ee2371ed8b8a9d8fffb0d1b8fbe8531ae5647
Andrew Nesbitt <andrewnez@gmail.com>
2010-04-12 23:25:13 +0100

    Basic list template improvements and validation

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,11 @@ class List < ActiveRecord::Base
   belongs_to :user
   has_many :gifts
 
+  validates_presence_of :title
+
   accepts_nested_attributes_for :gifts
+
+  def to_s
+    title
+  end
 end
