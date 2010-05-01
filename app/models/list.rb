25a25bedc35c89fd7ed0906200c19fe1b953f415
Andrew Nesbitt <andrewnez@gmail.com>
2010-05-01 15:11:57 +0100

    Ignore empty gifts

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,7 +4,7 @@ class List < ActiveRecord::Base
 
   validates_presence_of :title
 
-  accepts_nested_attributes_for :gifts
+  accepts_nested_attributes_for :gifts, :reject_if => proc { |attributes| attributes['title'].blank? }, :allow_destroy => true
 
   def to_s
     title
