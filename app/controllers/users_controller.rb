7e2762b48d5d3ffcef60a5ec8b7c68e80d79b561
Andrew Nesbitt <andrewnez@gmail.com>
2010-05-15 19:17:33 +0100

    redirect /account to root_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => [:show, :edit, :update]
 
+  def show
+    redirect_to root_path
+  end
+
   def new
     @user = User.new
   end
