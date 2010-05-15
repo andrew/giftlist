1357c39d171a5c8c0b09a9782fa5b46fec978683
Andrew Nesbitt <andrewnez@gmail.com>
2010-05-15 19:17:47 +0100

    Allow any method to the iframe view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :lists
 
-  map.resources :lists, :shallow => true, :member => {:iframe => :get} do |list|
+  map.resources :lists, :shallow => true, :member => {:iframe => :any} do |list|
     list.resources :gifts, :has_many => :reservations
   end
 
